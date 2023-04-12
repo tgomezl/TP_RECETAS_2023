c38143ea93ff7c69cc9ae144fb8c252b23af882e
tgomezl <82736073+tgomezl@users.noreply.github.com>
2023-04-12 15:39:26 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/TpRecetas2023Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpRecetas2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpRecetas2023Application.class, args);
+	}
+
+}
